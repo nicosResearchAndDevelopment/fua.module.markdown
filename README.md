7b2915db5d8789012284a1157defe14475f6d14a
Simon Petrac <pfoerdie@gmail.com>
2023-12-05 15:04:35 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # @nrd/fua.module.markdown
+
+- https://daringfireball.net/projects/markdown/
+- https://markdown.de/
+- https://www.markdownguide.org/
+
+
+- https://www.npmjs.com/package/markdown-it
+- https://markdown-it.github.io/markdown-it/
+- https://github.com/markdown-it/markdown-it/tree/master/lib
+- https://www.npmjs.com/package/highlight.js
---
+++ lab/example.js
@@ -0,0 +1,32 @@
+const
+    MarkdownIt = require('markdown-it'),
+    hljs       = require('highlight.js'),
+    md         = MarkdownIt('commonmark', {
+        highlight(str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(str, {language: lang, ignoreIllegals: true}).value;
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+
+            return ''; // use external default escaping
+        }
+    }),
+    mdZero     = MarkdownIt('zero');
+
+// mdZero.disable('paragraph');
+mdZero.renderer.rules.paragraph_open  = () => '';
+mdZero.renderer.rules.paragraph_close = () => '';
+
+const
+    input      = '# markdown-it rulezz!\n\nHello World!  \n`Lorem Ipsum`',
+    env        = {},
+    tokens     = md.parse(input, env),
+    output     = md.renderer.render(tokens, md.options, env),
+    zeroTokens = mdZero.parse(input, env),
+    zero       = mdZero.renderer.render(zeroTokens, mdZero.options, env);
+
+console.log({tokens, zeroTokens});
+console.table({input, output, zero});
